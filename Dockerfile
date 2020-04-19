2cafe3c22d304b1255fc9093ccdf95b0d366b2c9
Owen Smith <owen@omsmith.ca>
2020-04-18 21:24:17 -0400

    xo

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,9 @@
-FROM alpine:3.11
+FROM node:current-alpine
 
 ENV REVIEWDOG_VERSION=v0.9.17
 
-SHELL ["/bin/ash", "-eo", "pipefail", "-c"]
-
-# hadolint ignore=DL3006
-RUN apk --no-cache add git
-
 RUN wget -O - -q https://raw.githubusercontent.com/reviewdog/reviewdog/master/install.sh| sh -s -- -b /usr/local/bin/ ${REVIEWDOG_VERSION}
-
-# TODO: Install a linter and/or change docker image as you need.
-RUN wget -O - -q https://git.io/misspell | sh -s -- -b /usr/local/bin/
+RUN apk --no-cache add jq git
 
 COPY entrypoint.sh /entrypoint.sh
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -7,5 +7,23 @@ fi
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-misspell -locale="${INPUT_LOCALE}" . \
-  | reviewdog -efm="%f:%l:%c: %m" -name="linter-name (misspell)" -reporter="${INPUT_REPORTER:-github-pr-check}" -level="${INPUT_LEVEL}"
+if [ ! -f "$(npm bin)/xo" ]; then
+  if [ -f "package-lock.json" ]; then
+    npm ci
+  else
+    npm install
+  fi
+fi
+
+$(npm bin)/xo --version
+
+if [ "${INPUT_REPORTER}" == 'github-pr-review' ]; then
+  # Use jq and github-pr-review reporter to format result to include link to rule page.
+  $(npm bin)/xo --format json ${INPUT_XO_FLAGS:-'.'} \
+    | jq -r '.[] | {filePath: .filePath, messages: .messages[]} | "\(.filePath):\(.messages.line):\(.messages.column):\(.messages.message) [\(.messages.ruleId)](https://eslint.org/docs/rules/\(.messages.ruleId))"' \
+    | reviewdog -efm="%f:%l:%c:%m" -name="xo" -reporter=github-pr-review -level="${INPUT_LEVEL}"
+else
+  # github-pr-check,github-check (GitHub Check API) doesn't support markdown annotation.
+  $(npm bin)/xo --format stylish ${INPUT_XO_FLAGS:-'.'} \
+    | reviewdog -f="eslint" -name="xo" -reporter="${INPUT_REPORTER:-github-pr-check}" -level="${INPUT_LEVEL}"
+fi
